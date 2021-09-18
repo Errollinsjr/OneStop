440fa12580e6a6ed5be9e5e43376c64d76afc99d
Errollinsjr <errollins.jr@gmail.com>
2021-09-18 14:11:22 -0400

    final test home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import HomePage from "./components/HomePage";
+import HomePage from "./pages/HomePage";
 import Nav from "./components/Nav";
+// import "./App.scss";
 // import UserTripPage from "./components/UserTripPage";
 
 function App() {
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  // Gets all trips
+  getTrips: function() {
+    return axios.get("/api/trips");
   },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
+  // Gets the trip with the given id
+  getTrip: function(id) {
+    return axios.get("/api/trips/" + id);
   },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
+  // Deletes the trip with the given id
+  deleteTrip: function(id) {
+    return axios.delete("/api/trips/" + id);
   },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
+  // Saves a trip to the database
+  savetrip: function(tripData) {
+    return axios.post("/api/trips", tripData);
   }
 };
