41dfbe26adff6477dec0bf7deebeaf17798f8daf
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-09-21 21:03:25 -0400

    Added Trip.js to models.

---
+++ models/Trip.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//create Trip model
+class Trip extends Model {}
+
+//create columns for Trip model
+Trip.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        trip_name: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        start_date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        end_date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+                unique: false
+            }
+        },
+
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'trip'
+    }
+);
+
+module.exports = Trip;
