96bbfeef7dbf37d50babb255089dcf6094024d10
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-09-24 20:30:22 -0400

    Added /createtrip route.

--- routes/api/tripRoutes.js
+++ routes/api/tripRoutes.js
@@ -22,4 +22,22 @@ router.get('/usertrips', withAuth, async(req, res) => {
     }
 });
 
+//POST create user trip
+router.post('/createtrip', async(req, res) => {
+    try {
+        //create new trip
+        await Trip.create({
+            trip_name: req.body.trip_name,
+            start_date: req.body.start_date,
+            end_date: req.body.end_date,
+            tags: req.body.tags,
+            upload: req.body.upload,
+            user_id: req.session.user_id
+        })
+        res.status(200).json({ message: 'Trip created.'})
+    } catch (err) {
+        res.status(400).json({ error: err })
+    };
+});
+
 module.exports = router;
