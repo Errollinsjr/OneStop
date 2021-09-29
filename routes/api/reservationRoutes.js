9a50c360ac31ecf237185be2d6f9a82a710f4989
melissarmand <82781751+melissarmand@users.noreply.github.com>
2021-09-28 20:43:45 -0400

    functioning trips

--- routes/api/reservationRoutes.js
+++ routes/api/reservationRoutes.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const { Reservations, Trip } = require("../../models");
-const withAuth = require("../../utils/auth.js");
 
-router.get('/userreservations', withAuth, async(req, res) => {
+
+router.get('/:trip_id', async(req, res) => {
     try {
         const tripDetails = await Trip.findOne({
-            where: {id: req.session.trip_id},
+            where: {id: req.params.trip_id},
             include: [
                 {
                     model: Reservations
@@ -27,7 +27,8 @@ router.post('/createreservation', async(req, res) => {
             name: req.body.name,
             confirmation: req.body.confirmation,
             description: req.body.description,
-            trip_id: req.session.trip_id
+            trip_id: req.body.trip_id,
+            
         })
         res.status(200).json({ message: 'Reservation created!'})
     }catch (err) {
@@ -35,62 +36,45 @@ router.post('/createreservation', async(req, res) => {
     };
 });
 
-router.put ('/:tripd_id', async(req, res) => {
+router.put('/:reservations_id', async(req, res) => {
     try {
         console.log(req.body)
         console.log(req.params.id)
 
         const editedReservation = await Reservations.update(
-            {}
+            {
+            type: req.body.type,
+            name: req.body.name,
+            confirmation: req.body.confirmation,
+            description: req.body.description,
+            trip_id: req.session.trip_id
+
+            }, 
+            { where:{
+                id: req.params.reservations_id
+            }
+               
+            },
         )
+        res.status(200).json(editedReservation);
+    } catch (err) {
+        res.status(400).json({ error: err})
+    }
+});
+
+router.delete('/:reservations_id', async (req, res) => {
+    try {
+        const deletedReservation = await Reservations.destroy({
+            where: {
+                id: req.params.reservations_id,
+            },
+        });
+        res.status(200).json(deletedReservation)
+    } catch (err) {
+        res.status(400).json({ error: err})
     }
 })
 
-
-// //PUT (edit) user trip
-// router.put('/:trip_id', async(req, res) => {
-//     try {
-//         console.log(req.body)
-//         console.log(req.params.id)
-//         //call update method on trip model
-//         const editedTrip = await Trip.update(
-//             {
-//                 trip_name: req.body.trip_name,
-//                 start_date: req.body.start_date,
-//                 end_date: req.body.end_date,
-//                 tags: req.body.tags,
-//                 upload: req.body.upload,
-//             },
-//             {
-//                 //get trip based on id in request parameter
-//                 where: {
-//                     id: req.params.trip_id
-//                 },
-//             }
-//         )
-//         res.status(200).json(editedTrip);
-//     } catch (err) {
-//         res.status(400).json({ error: err })
-//     }
-// })
-
-// //DELETE user trip
-// router.delete('/:trip_id', async (req, res) => {
-//     try {
-//         //look for trip id given in request param
-//         const deletedTrip = await Trip.destroy({
-//             where: {
-//                 id: req.params.trip_id,
-//             },
-//         });
-
-//         res.status(200).json(deletedTrip)
-//     } catch (err) {
-//         res.status(400).json({ error: err })
-//     }
-
-
-// })
 
 
 module.exports = router
