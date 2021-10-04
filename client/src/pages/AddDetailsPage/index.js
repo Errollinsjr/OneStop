de35efab3a29a453bf9b29ebc8ceffd94120faf7
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-10-03 20:14:37 -0400

    Modified Add Reservation Detail styling to make it responsive.

--- client/src/pages/AddDetailsPage/index.js
+++ client/src/pages/AddDetailsPage/index.js
@@ -53,98 +53,97 @@ function AddDetailsPage() {
 
   return (
     <>
-    <div className='back1'>
-    <div className="res-det-container ">
-        <div className=" row justify-content-center">
-            <div className="col-lg-5">
-                <div className="card shadow-lg border-0 rounded-lg mt-6">
-                    <div className="card-header header-color"><h3 className="text-center font-weight-light my-4">Add Reservation Details</h3></div>
+    <div className='back1 height'>
+        {/* <div className="res-det-container "> */}
+            <div className=" row justify-content-center">
+                <div className="col-9 col-sm-7 col-md-5 col-lg-5">
+                    <div className="card shadow-lg border-0 rounded-lg mt-5 reservation-detail">
+                        <div className="card-header header-color">
+                            <h3 className="text-center font-weight-light my-4">Add Reservation Details</h3>
+                        </div>
                         <div className="card-body">
                             
                             <form 
                                 id="reservationForm"
                                 method= "post"
-                                action = "submit">
+                                action = "submit"
+                            >
 
-                            <div className="form-floating mb-3">
-                            <label>Reservation Type</label>
-                                <input 
-                                    className="form-control input-color" 
-                                    id="inputFieldAddDetails" 
-                                    type="text" 
-                                    name="type" 
-                                    placeholder="Reservation Name" 
-                                    value={formObject.type}
-                                    onChange={ handleInputChange }
-                                />
-                            </div>
-                            <div className="form-floating mb-3">
-                            <label>Reservation Name</label>
-                                <input 
-                                    className="form-control input-color" 
-                                    id="inputFieldAddDetails" 
-                                    type="text" 
-                                    name="name" 
-                                    placeholder="Reservation Name" 
-                                    value={formObject.name}
-                                    onChange={ handleInputChange }
-                                />
-                            </div>
-                            <div className="form-floating mb-3">
-                            <label>Confirmation Number</label>
-                                <input 
-                                    className="form-control input-color" 
-                                    id="inputFieldAddDetails" 
-                                    type="text" 
-                                    name="confirmation"
-                                    placeholder="Confirmation Number"
-                                    value={formObject.confirmation}
-                                    onChange={handleInputChange}
+                                <div className="form-floating mb-3">
+                                    <label>Reservation Type</label>
+                                    <input 
+                                        className="form-control input-color" 
+                                        id="inputFieldAddDetails" 
+                                        type="text" 
+                                        name="type" 
+                                        placeholder="Reservation Name" 
+                                        value={formObject.type}
+                                        onChange={ handleInputChange }
                                     />
-                                
-                            </div>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <label>Reservation Name</label>
+                                    <input 
+                                        className="form-control input-color" 
+                                        id="inputFieldAddDetails" 
+                                        type="text" 
+                                        name="name" 
+                                        placeholder="Reservation Name" 
+                                        value={formObject.name}
+                                        onChange={ handleInputChange }
+                                    />
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <label>Confirmation Number</label>
+                                    <input 
+                                        className="form-control input-color" 
+                                        id="inputFieldAddDetails" 
+                                        type="text" 
+                                        name="confirmation"
+                                        placeholder="Confirmation Number"
+                                        value={formObject.confirmation}
+                                        onChange={handleInputChange}
+                                        />
+                                </div>
 
-                            <div className="form-floating mb-3">
-                                <label>Description</label>
-                                <input 
-                                    className="form-control input-color" 
-                                    id="inputFieldAddDetails" 
-                                    type="text"
-                                    name="description"
-                                    placeholder="Description or Link"
-                                    value={formObject.description}
-                                    onChange={handleInputChange}
-                                />
-                            </div>
-                            
-                            <div className="form-floating mb-3">
-                                <input
-                                    className= "btn btn-primary btn-sm"
-                                    type= "submit"
-                                    value="Submit"
-                                    onClick= {handleFormSubmit}
-                                />
-                            </div>
-
-                        </form>                                                                         
+                                <div className="form-floating mb-3">
+                                    <label>Description</label>
+                                    <input 
+                                        className="form-control input-color" 
+                                        id="inputFieldAddDetails" 
+                                        type="text"
+                                        name="description"
+                                        placeholder="Description or Link"
+                                        value={formObject.description}
+                                        onChange={handleInputChange}
+                                    />
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <input
+                                        className= "btn btn-primary btn-sm"
+                                        type= "submit"
+                                        value="Submit"
+                                        onClick= {handleFormSubmit}
+                                    />
+                                </div>
+                            </form>                                                                         
                         </div>
-                    
-                    {/* <ReservationEditButton onClick={handleShow} >View Reservations</ReservationEditButton>                     */}
-                    <Modal onClose={handleClose} show={show}/>
-
-                    <div className="card-footer text-center py-3">
-                    <button 
-                        className="userTripPageButton btn btn-primary btn-sm" 
-                        onClick={() => history.push("/Summary/" + formObject.trip_id)}>
-                        Trip Summary
-                    </button>
-                    <ReservationEditButton onClick={handleShow} >View Reservations</ReservationEditButton>
+                        
+                        <div className="card-footer text-center py-3 d-flex justify-content-between reservation-footer">
+                            <button 
+                                className="userTripPageButton btn btn-primary btn-sm" 
+                                onClick={() => history.push("/Summary/" + formObject.trip_id)}>
+                                Trip Summary
+                            </button>
+                            <ReservationEditButton onClick={handleShow}>View Reservations</ReservationEditButton>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        {/* </div> */}
     </div>
-    </div>
+
+    <Modal onClose={handleClose} show={show}/>
     </>
   );
 }
